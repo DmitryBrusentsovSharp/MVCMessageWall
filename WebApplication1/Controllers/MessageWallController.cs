d3f60e15de09e8db263aae6e6834bb786130f2b6
dmitry.brusentsov <dmitry.brusentsov@sharp-dev.net>
2024-01-25 12:32:22 +0800

    first commit

---
+++ WebApplication1/Controllers/MessageWallController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MessageWallController : Controller
+    {
+        // GET: MessageWallController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MessageWallController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MessageWallController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MessageWallController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MessageWallController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MessageWallController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MessageWallController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MessageWallController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/MessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace WebApplication1.Models
+{
+    public class MessageModel
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 8)]
+        [DefaultValueAttribute("User")]
+        public string Message { get; set; }
+
+        public MessageModel() {
+        }
+    }
+}
